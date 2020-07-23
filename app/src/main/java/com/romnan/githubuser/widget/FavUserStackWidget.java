b9407398294144e5160c92a112a4d3979fe1097b
Deddy Romnan <romnandeddy@gmail.com>
2020-07-23 16:40:33 +0700

    added widget

--- app/src/main/java/com/romnan/githubuser/widget/FavUserStackWidget.java
+++ app/src/main/java/com/romnan/githubuser/widget/FavUserStackWidget.java
@@ -1,4 +1,4 @@
-package com.romnan.githubuser;
+package com.romnan.githubuser.widget;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
 import android.widget.Toast;
+
+import com.romnan.githubuser.R;
 
 public class FavUserStackWidget extends AppWidgetProvider {
 
---
+++ app/src/main/java/com/romnan/githubuser/widget/StackRemoteViewsFactory.java
@@ -0,0 +1,78 @@
+package com.romnan.githubuser.widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.romnan.githubuser.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private final List<Bitmap> mWidgetItems = new ArrayList<>();
+    private final Context mContext;
+
+    public StackRemoteViewsFactory(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.getResources(), R.drawable.img_profile));
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return mWidgetItems.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
+        rv.setImageViewBitmap(R.id.imageView, mWidgetItems.get(position));
+
+        Bundle extras = new Bundle();
+        extras.putInt(FavUserStackWidget.EXTRA_ITEM, position);
+        Intent fillInIntent = new Intent();
+        fillInIntent.putExtras(extras);
+
+        rv.setOnClickFillInIntent(R.id.imageView, fillInIntent);
+        return rv;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+}
