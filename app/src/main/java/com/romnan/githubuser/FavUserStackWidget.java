59d1c3249e60b45daac88c7ab76950537d577d52
Deddy Romnan <romnandeddy@gmail.com>
2020-07-23 16:26:33 +0700

    added widget

---
+++ app/src/main/java/com/romnan/githubuser/FavUserStackWidget.java
@@ -0,0 +1,58 @@
+package com.romnan.githubuser;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class FavUserStackWidget extends AppWidgetProvider {
+
+    public static final String EXTRA_ITEM = "com.romnan.EXTRA_ITEM";
+    private static final String TOAST_ACTION = "com.romnan.TOAST_ACTION";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Intent intent = new Intent(context, FavUserStackWidget.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.fav_user_stack_widget);
+        views.setRemoteAdapter(R.id.stack_view, intent);
+        views.setEmptyView(R.id.stack_view, R.id.empty_view);
+
+        Intent toastIntent = new Intent(context, FavUserStackWidget.class);
+        toastIntent.setAction(FavUserStackWidget.TOAST_ACTION);
+        toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0,
+                toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction() != null) {
+            if (intent.getAction().equals(TOAST_ACTION)) {
+                int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+                Toast.makeText(context, "Touched view " + viewIndex, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
+
